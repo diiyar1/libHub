7d43c29ebfea6de7c660c783bfafb05f8c707eae
diiyar <adamsanna015@gmail.com>
2024-06-28 23:44:25 +0300

    library comit
    # library
     a library website made with nuxt3, tailwind css, supabase
     -json-server data.json --port 3001
    -npm run dev
    
    library website made with nuxt3, tailwind css, supabase
    it has registerartion, login and out , search, add to favourite, selecet genres

---
+++ composables/searchBooks.ts
@@ -0,0 +1,17 @@
+// composables/searchBooks.ts
+import { useBooksStore } from '~/composables/getBooks'
+
+export function useSearchBooks() {
+  const { searchQuery, filteredBooks, fetchBooks } = useBooksStore()
+
+  const searchBooks = async (query: string) => {
+    searchQuery.value = query
+    await fetchBooks(query)
+  }
+
+  return {
+    searchQuery,
+    filteredBooks,
+    searchBooks,
+  }
+}
